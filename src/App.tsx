6f028966c12595e24e4213b764a2a75a4d9185a5
nachomartinez3010 <nachomartinez3010@gmail.com>
2024-05-01 16:52:41 -0600

    removing react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
-import { useContext, useState } from "react";
+import { useContext, useRef } from "react";
 import { Button, Form } from "react-bootstrap";
 import { ModalContext } from "./context/ModalContext";
-import { useForm } from "react-hook-form";
 
 function App() {
   const modalCtx = useContext(ModalContext);
-  const [data, setData] = useState<string>("");
-  const form = useForm({
-    defaultValues: {
-      searchValue: "",
-    },
-  });
+  const searchInput = useRef<HTMLInputElement>(null);
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(searchInput.current?.value);
+  };
 
   const handleCreateModal = () => {
     modalCtx!.createModal({
@@ -26,20 +25,21 @@ function App() {
     modalCtx!.handleOpen();
   };
 
-  const MyForm = () => (
-    <Form onSubmit={form.handleSubmit((data) => setData(JSON.stringify(data)))}>
-      <Form.Group>
-        <Form.Label>Label</Form.Label>
-        <input {...form.register("searchValue")} />
-        <Button type="submit">Search</Button>
-      </Form.Group>
-    </Form>
-  );
+  const MyForm = () => {
+    return (
+      <Form onSubmit={handleOnSubmit}>
+        <Form.Group>
+          <Form.Label>Label</Form.Label>
+          <input ref={searchInput} />
+          <Button type="submit">Search</Button>
+        </Form.Group>
+      </Form>
+    );
+  };
 
   return (
     <div>
       <Button onClick={handleCreateModal}>Create Modal</Button>
-      {data && <p>{data}</p>}
     </div>
   );
 }
